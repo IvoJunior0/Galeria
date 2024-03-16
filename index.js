f6e3b8a7af1d4e5a03a87ba4f592a1e6f56ffa59
IvoJunior0 <shymanjr2019@gmail.com>
2024-03-16 18:58:38 -0300

    sessão de vídeos adicionada

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// FAZ O POP-UP DAS IMAGENS
 $(document).ready(function() {
     $('.fotos').magnificPopup({
         delegate: 'a',
@@ -6,8 +7,15 @@ $(document).ready(function() {
             enabled:true
         }
     })
+    $('.videos').magnificPopup({
+        delegate: 'a',
+        type: 'iframe',
+        gallery:{
+            enabled:true
+        }
+    })
 });
-
+// ADICIONA A CLASSE ANIMATE PARA ANIMAR NO SCROLL
 const animationElements = document.querySelectorAll('.animate-on-scroll');
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -20,7 +28,7 @@ const observer = new IntersectionObserver((entries) => {
 }, {
     threshold: 0.5
 });
-
+// OBSERVA CADA ELEMENTO COM A CLASSE .animate-on-scroll
 for (let i = 0; i < animationElements.length; i++){
     const el = animationElements[i];
     observer.observe(el);
