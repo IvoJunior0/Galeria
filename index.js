26732e9708bd6fa39e4b89cfd6f06be874e9573c
IvoJunior0 <shymanjr2019@gmail.com>
2024-03-16 15:52:40 -0300

    adicionando animações trigger no scroll

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ $(document).ready(function() {
         }
     })
 });
+
+const animationElements = document.querySelectorAll('.animate-on-scroll');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+for (let i = 0; i < animationElements.length; i++){
+    const el = animationElements[i];
+    observer.observe(el);
+}
